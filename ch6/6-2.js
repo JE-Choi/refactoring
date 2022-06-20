d9e3fae0f8f8bd0051ba8dcc40ffc08f94bf29c7
최지은 <jechoi@miridih.com>
2022-06-20 20:17:10 +0900

    6.2 함수 인라인하기

--- ch6/6-2.js
+++ ch6/6-2.js
@@ -1,17 +1,25 @@
 // 예제 1
 export function rating(driver) {
-  return moreThanFiveLateDeliveries(driver) ? 2 : 1;
+  return driver.numberOfLateDeliveries > 5  ? 2 : 1;
 }
 
-function moreThanFiveLateDeliveries(dvr) {
+/**
+ * 좀더 추상화된 이름이거나 재사용이 필요하다면 함수로 추출
+ * - 아니면 굳이?
+ */
+const isBestDriver = (dvr) => {
   return dvr.numberOfLateDeliveries > 5;
 }
 
-// 예제 2
+// 예제 2 - 너무 세밀하게 나눠진 함수
+/**
+ * Q: 6. 함수 인라인하기 - 만약에 gatherCustomerData 내용이 엄청 길었다면? [#1](https://github.com/JE-Choi/refactoring/issues/1)
+ */
 function reportLines(customer) {
-  const lines = [];
-  gatherCustomerData(lines, customer);
-  return lines;
+  const result = [];
+  result.push(['name', customer.name]);
+  result.push(['location', customer.location]);
+  return result;
 }
 
 function gatherCustomerData(out, customer) {
