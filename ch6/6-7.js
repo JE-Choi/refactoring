938b2ec384bd629be9ac927456b8d7091f4988d5
최지은 <jechoi@miridih.com>
2022-06-23 00:00:41 +0900

    6.7 변수 이름 바꾸기

--- ch6/6-7.js
+++ ch6/6-7.js
@@ -1,6 +1,9 @@
-let a = height * width;
+// a라는 변수는 가독성이 없다. (a → area)
+let area = width * height; // 너비와 높이라고 자주 말하니까, 순서변경
 
-const cpyNm = '애플';
+// 축약어 지양
+const companyName = '애플';
 
-let tpHd = '제목없음';
-let result = `<h1>${tpHd}</h1>`;
+// 유추해야 하는 이름 지양 → 구체적으로 작성
+let movieTitle = '제목없음';
+let result = `<h1>${movieTitle}</h1>`;
