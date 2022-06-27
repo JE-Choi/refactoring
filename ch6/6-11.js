ddd818d66d3246b4c6bb916bfc907837ec8fd293
최지은 <jechoi@miridih.com>
2022-06-27 21:24:29 +0900

    6.11 단계쪼개기1

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,30 +1,56 @@
-export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
-}
-
 // 사용 예:
 const product = {
-  basePrice: 10,
-  discountRate: 0.1,
-  discountThreshold: 10,
+    basePrice: 10,
+    discountRate: 0.1,
+    discountThreshold: 10,
 };
 
 const shippingMethod = {
-  discountThreshold: 20,
-  feePerCase: 5,
-  discountedFee: 3,
+    discountThreshold: 20,
+    feePerCase: 5,
+    discountedFee: 3,
 };
 
-const price = priceOrder(product, 5, shippingMethod);
-console.log(price);
+class Order {
+  #product;
+  #shippingMethod;
+  #quantity;
+
+  constructor({product, shippingMethod, quantity}) {
+    this.#product = product;
+    this.#shippingMethod = shippingMethod;
+    this.#quantity = quantity;
+  }
+
+  // 기본 제품 가격
+  get basePrice() {
+    return this.#product.basePrice * this.#quantity;
+  }
+
+  // 할인률
+  get discount() {
+    return Math.max(this.#quantity - this.#product.discountThreshold, 0) *
+        this.#product.basePrice *
+        this.#product.discountRate;
+  }
+
+  // 개별 배송비 계산
+  get shippingPerCase() {
+    return this.basePrice > this.#shippingMethod.discountThreshold
+        ? this.#shippingMethod.discountedFee
+        : this.#shippingMethod.feePerCase;
+  }
+
+  // 총 배송비 계산 
+  get shippingCost() {
+    return this.#quantity * this.shippingPerCase;
+  }
+
+  // 총 가격
+  get priceOrder() {
+    return this.basePrice - this.discount + this.shippingCost;
+  }
+}
+
+let order = new Order({product, quantity: 5, shippingMethod});
+console.log(order.priceOrder); // 65
