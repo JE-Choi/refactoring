fc7904ddc5e226dd30840899fd41f1a56abdd88f
최지은 <jechoi@miridih.com>
2022-07-03 17:22:23 +0900

    7.3 기본형을 객체로 바꾸기-1

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -1,15 +1,36 @@
-export class Order {
-  constructor(data) {
-    this.priority = data.priority;
+/**
+ * 7.3 기본형을 객체로 바꾸기
+ */
+class Priority {
+    #level;
+
+  constructor(priority) {
+    this.#level = priority;
+  }
+
+  isHigh = () => {
+    return 'high' === this.#level || 'rush' === this.#level;
   }
 }
 
+export class Order {
+    #priority;
+    constructor(data) {
+        this.#priority = data.priority;
+    }
+
+    get priority(){
+      return this.#priority;
+    }
+}
+
 const orders = [
-  new Order({ priority: 'normal' }),
-  new Order({ priority: 'high' }),
-  new Order({ priority: 'rush' }),
+    new Order({priority: new Priority('normal')}),
+    new Order({priority: new Priority('high')}),
+    new Order({priority: new Priority('rush')}),
 ];
 
 const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
+    (o) => o.priority.isHigh()
 ).length;
+console.log(highPriorityCount);
