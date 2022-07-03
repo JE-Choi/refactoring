42a1d586ca3c2b725a6a1c2c502e8b739db12883
최지은 <jechoi@miridih.com>
2022-07-03 18:05:48 +0900

    7.7 클래스 추출하기

--- ch7/7-6.js
+++ ch7/7-6.js
@@ -1,4 +1,8 @@
-export class TrackingInformation {
+/**
+ * 7.6 클래스 인라인하기
+ * 역할이 없는데, class화 되어있고 위임만 하는 경우에는 통합하는게 나을 수 있다.
+ */
+export class Shipment {
   #shippingCompany;
   #trackingNumber;
   constructor(trackingNumber, shippingCompany) {
@@ -27,27 +31,8 @@ export class TrackingInformation {
   }
 }
 
-export class Shipment {
-  #trackingInformation;
-  constructor(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
+const shipment = new Shipment(999, 'Maersk');
+console.log(shipment.display);
 
-  get trackingInfo() {
-    return this.#trackingInformation.display;
-  }
-
-  get trackingInformation() {
-    return this.#trackingInformation;
-  }
-
-  set trackingInformation(trackingInformation) {
-    this.#trackingInformation = trackingInformation;
-  }
-}
-
-const shipment = new Shipment(new TrackingInformation(999, 'Maersk'));
-console.log(shipment.trackingInfo);
-
-shipment.trackingInformation.shippingCompany = 'COSCO';
-console.log(shipment.trackingInfo);
+shipment.shippingCompany = 'COSCO';
+console.log(shipment.display);
