8f7015fe1fdd1546123e1f8e545759122c2dc2dd
최지은 <jechoi@miridih.com>
2022-07-03 18:25:18 +0900

    7.9 알고리즘 교체하기

--- ch7/7-9.js
+++ ch7/7-9.js
@@ -1,16 +1,10 @@
+/**
+ * 7.9 알고리즘 교체하기
+ * - 알고리즘을 교체할 때는, 해당 함수의 테스트코드가 있어야한다.
+ */
 function foundPerson(people) {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don';
-    }
-    if (people[i] === 'John') {
-      return 'John';
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent';
-    }
-  }
-  return '';
+  const candidates = ['Don', 'John', 'Kent'];
+  return people.find((p) => candidates.includes(p) || '');
 }
 
 console.log(foundPerson(['John']));
