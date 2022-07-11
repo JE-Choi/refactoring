81363e3100435e0f2edd8dd277ef5f1f38e64941
최지은 <jechoi@miridih.com>
2022-07-11 19:30:23 +0900

    8.2 필드 옮기기2 (완)

--- ch8/8-2-2.js
+++ ch8/8-2-2.js
@@ -1,17 +1,30 @@
+/**
+ * 8.2 필드 옮기기
+ */
 class Account {
-  constructor(number, type, interestRate) {
+  constructor(number, type) {
     this._number = number;
     this._type = type;
-    this._interestRate = interestRate;
+
   }
 
   get interestRate() {
-    return this._interestRate;
+    return this.type.interestRate;
   }
 }
 
 class AccountType {
-  constructor(nameString) {
+  constructor(nameString, interestRate) {
     this._name = nameString;
+    this._interestRate = interestRate;
+    this._nameString = nameString;
+  }
+
+  get nameString() {
+    return this._nameString;
+  }
+
+  get interestRate() {
+    return this._interestRate;
   }
 }
